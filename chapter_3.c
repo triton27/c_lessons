199425e9aa60a60c19b2d96664d6e38bee192754
toriumi <na54heart@icloud.com>
2019-04-02 02:06:43 +0900

    Add chapter_3 for learn about variable

---
+++ chapter_3.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+/*
+変数: データにつけるラベル
+
+データ型
+int %d
+float %f
+char(1文字) %c
+
+*/
+
+int main(void) {
+
+        int x = 10;
+        float y = 5.3;
+        char c = 'A';
+
+        printf("x = %d, y = %f, c = %c\n", x, y, c);
+        return 0;
+}
