913535278bac3519180c763f439c3b6e91213fae
toriumi <na54heart@icloud.com>
2019-04-04 23:53:16 +0900

    Add chapter_14 for array

---
+++ chapter_14.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+
+/*
+
+配列
+
+*/
+
+int main(void) {
+
+    int sales[3];
+
+    sales[0] = 200;
+    sales[1] = 400;
+    sales[2] = 300;
+
+    int sales2[3] = {200, 400, 300};
+
+    printf("%d\n", sales[2]);
+
+    return 0;
+}
+
